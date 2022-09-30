6242f4044c81156cc2323f8e3f459ca2aa24bc60
DESKTOP-7NBOJSE\Random <vantri_antonius@yahoo.co.id>
2022-09-30 22:48:40 +0700

    Caridosen

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codeigniter3/caridosen/';
+$config['base_url'] = 'http://localhost/caridosen/';
 
 /*
 |--------------------------------------------------------------------------
